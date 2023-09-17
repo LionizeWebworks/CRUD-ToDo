438cef36fc7839e74a1eb0b965dc6d555c2fc420
LionizeWebworks <lionizewebworks@gmail.com>
2023-09-17 10:05:24 -0500

    First commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("form");
+gconst form = document.getElementById("form");
 const input = document.getElementById("task"); // Corrected the id to "task"
 const tasks = document.getElementById("task-list"); // Added this line to select the task list
 const msg = document.getElementById("msg");
